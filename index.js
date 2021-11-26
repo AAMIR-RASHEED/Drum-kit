e9068f379cc61d1cca67758f9ac8f52d7f911ee8
mohdaamir <mohdaamir5975@gmail.com>
2021-11-26 21:58:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+// Detect button press
+var numberOfButton = document.querySelectorAll(".drum").length;
+for(var i=0; i<numberOfButton; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
+
+var buttonInnerHtml=this.innerHTML;
+makeSound(buttonInnerHtml);
+animation(buttonInnerHtml);
+
+});
+
+}
+// Detect keyboard press
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  animation(event.key);
+})
+
+function makeSound(key){
+
+  switch (key) {
+    case "w":
+      var tom1=new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+      case "a":
+        var tom2=new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+        case "s":
+          var tom3=new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+
+          case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+            case "j":
+              var tom5=new Audio("sounds/snare.mp3");
+              tom5.play();
+              break;
+
+              case "k":
+                var tom6=new Audio("sounds/crash.mp3");
+                tom6.play();
+                break;
+
+                case "l":
+                  var tom6=new Audio("sounds/kick-bass.mp3");
+                  tom6.play();
+                  break;
+    default:
+  }
+}
+
+function animation(currentKey){
+var activeButton= document.querySelector("." + currentKey);
+activeButton.classList.add("pressed");
+setTimeout(function(){
+  activeButton.classList.remove("pressed");
+},100);
+}
